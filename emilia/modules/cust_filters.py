158b69b1c551a18afffa1549b766650a66f8eff4
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2020-04-22 01:07:43 +0530

    f.___xD

--- emilia/modules/cust_filters.py
+++ emilia/modules/cust_filters.py
@@ -72,9 +72,9 @@ def list_handlers(update, context):
                 else:
                         filter_list += entry
         try:
-		send_message(update.effective_message, filter_list.format(chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
+                send_message(update.effective_message, filter_list.format(chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
 	except KeyError:
-		pass
+                pass
 
 
 # NOT ASYNC BECAUSE DISPATCHER HANDLER RAISED
