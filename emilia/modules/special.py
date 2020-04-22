4ced7f375e71cc46859c348dbc6d20c7a7ce0c33
Prakasaka - the illuminator <heartbeatmwa@gmail.com>
2020-04-22 21:16:26 +0530

    f.___xD wiki & ud

--- emilia/modules/special.py
+++ emilia/modules/special.py
@@ -11,7 +11,6 @@ import urbandict
 
 import pyowm
 from pyowm import timeutils, exceptions
-from googletrans import Translator
 import wikipedia
 import base64
 from bs4 import BeautifulSoup
@@ -123,155 +122,24 @@ def leavechat(update, context):
 			return
 
 
-
-@run_async
-def trans_late(update, context):
-	msg = update.effective_message
-	chat_id = update.effective_chat.id
-	getlang = langsql.get_lang(update.effective_message.from_user.id)
-	try:
-		if msg.reply_to_message and msg.reply_to_message.text:
-			args = update.effective_message.text.split()
-			if len(args) >= 2:
-				target = args[1]
-				if "-" in target:
-					target2 = target.split("-")[1]
-					target = target.split("-")[0]
-				else:
-					target2 = None
-			else:
-				if getlang:
-					target = getlang
-					target2 = None
-				else:
-					raise IndexError
-			teks = msg.reply_to_message.text
-			#teks = deEmojify(teks)
-			exclude_list = UNICODE_EMOJI.keys()
-			for emoji in exclude_list:
-				if emoji in teks:
-					teks = teks.replace(emoji, '')
-			message = update.effective_message
-			trl = Translator()
-			if target2 == None:
-				deteksibahasa = trl.detect(teks)
-				tekstr = trl.translate(teks, dest=target)
-				send_message(update.effective_message, "Translated from `{}` to `{}`:\n`{}`".format(deteksibahasa.lang, target, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-			else:
-				tekstr = trl.translate(teks, dest=target2, src=target)
-				send_message(update.effective_message, "Translated from `{}` to `{}`:\n`{}`".format(target, target2, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-			
-		else:
-			args = update.effective_message.text.split(None, 2)
-			if len(args) != 1:
-				target = args[1]
-				teks = args[2]
-				target2 = None
-				if "-" in target:
-					target2 = target.split("-")[1]
-					target = target.split("-")[0]
-			else:
-				target = getlang
-				teks = args[1]
-			#teks = deEmojify(teks)
-			exclude_list = UNICODE_EMOJI.keys()
-			for emoji in exclude_list:
-				if emoji in teks:
-					teks = teks.replace(emoji, '')
-			message = update.effective_message
-			trl = Translator()
-			if target2 == None:
-				deteksibahasa = trl.detect(teks)
-				tekstr = trl.translate(teks, dest=target)
-				return send_message(update.effective_message, "Translated from `{}` to `{}`:\n`{}`".format(deteksibahasa.lang, target, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-			else:
-				tekstr = trl.translate(teks, dest=target2, src=target)
-				send_message(update.effective_message, "Translated from `{}` to `{}`:\n`{}`".format(target, target2, tekstr.text), parse_mode=ParseMode.MARKDOWN)
-	except IndexError:
-		send_message(update.effective_message, "Reply to messages or write messages from other languages ​​to translate into the intended language\n\nExample: `/tr en-id` to translate from English to Indonesian\nOr use: `/tr id` for automatic detection and translating it into Indonesian", parse_mode="markdown")
-	except ValueError:
-		send_message(update.effective_message, "The destination language is not found!")
-	else:
-		return
-
-
 @run_async
 def wiki(update, context):
-	msg = update.effective_message
-	chat_id = update.effective_chat.id
-	args = update.effective_message.text.split(None, 1)
-	teks = args[1]
-	message = update.effective_message
-	getlang = langsql.get_lang(chat_id)
-	if str(getlang) == "id":
-		wikipedia.set_lang("id")
-	else:
-		wikipedia.set_lang("en")
-	try:
-		pagewiki = wikipedia.page(teks)
-	except wikipedia.exceptions.PageError:
-		send_message(update.effective_message, "Results not found")
-		return
-	except wikipedia.exceptions.DisambiguationError as refer:
-		rujuk = str(refer).split("\n")
-		if len(rujuk) >= 6:
-			batas = 6
-		else:
-			batas = len(rujuk)
-		teks = ""
-		for x in range(batas):
-			if x == 0:
-				if getlang == "id":
-					teks += rujuk[x].replace('may refer to', 'dapat merujuk ke')+"\n"
-				else:
-					teks += rujuk[x]+"\n"
-			else:
-				teks += "- `"+rujuk[x]+"`\n"
-		send_message(update.effective_message, teks, parse_mode="markdown")
-		return
-	except IndexError:
-		send_message(update.effective_message, "Write a message to search from the wikipedia source")
-		return
-	judul = pagewiki.title
-	summary = pagewiki.summary
-	if update.effective_message.chat.type == "private":
-		send_message(update.effective_message, "Results of {} is:\n\n<b>{}</b>\n{}".format(teks, judul, summary), parse_mode=ParseMode.HTML)
-	else:
-		if len(summary) >= 200:
-			judul = pagewiki.title
-			summary = summary[:200]+"..."
-			button = InlineKeyboardMarkup([[InlineKeyboardButton(text="Read on Wikipedia", url="t.me/{}?start=wiki-{}".format(context.bot.username, teks.replace(' ', '_')))]])
-		else:
-			button = None
-		send_message(update.effective_message, "Results of {} is:\n\n<b>{}</b>\n{}".format(teks, judul, summary), parse_mode=ParseMode.HTML, reply_markup=button)
-
+    args = context.args
+    reply = " ".join(args)
+    summary = f"{wikipedia.summary(reply, sentences=3)} [More]({wikipedia.page(reply).url})"
+    send_message(update.effective_message, summary, parse_mode=ParseMode.MARKDOWN)
 
 @run_async
 def urbandictionary(update, context):
-	args = context.args
-	msg = update.effective_message
-	chat_id = update.effective_chat.id
-	message = update.effective_message
-	if args:
-		text = " ".join(args)
-		try:
-			mean = urbandict.define(text)
-		except Exception as err:
-			send_message(update.effective_message, "Error: " + str(err))
-			return
-		if len(mean) >= 0:
-			teks = ""
-			if len(mean) >= 3:
-				for x in range(3):
-					teks = "*Result of {}*\n\n*{}*\n*Meaning:*\n`{}`\n\n*Example:*\n`{}`\n\n".format(text, mean[x].get("word")[:-7], mean[x].get("def"), mean[x].get("example"))
-			else:
-				for x in range(len(mean)):
-					teks = "*Result of {}*\n\n*{}*\n**Meaning:*\n`{}`\n\n*Example:*\n`{}`\n\n".format(text, mean[x].get("word")[:-7], mean[x].get("def"), mean[x].get("example"))
-			send_message(update.effective_message, teks, parse_mode=ParseMode.MARKDOWN)
-		else:
-			send_message(update.effective_message, "{} couldn't be found in urban dictionary!".format(text), parse_mode=ParseMode.MARKDOWN)
-	else:
-		send_message(update.effective_message, "Use `/ud <text` for search meaning from urban dictionary.", parse_mode=ParseMode.MARKDOWN)
+    message = update.effective_message
+    text = message.text[len('/ud '):]
+    results = requests.get(f'http://api.urbandictionary.com/v0/define?term={text}').json()
+    try:
+        reply_text = f'*{text}*\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
+    except:
+        reply_text = "No results found."
+    send_message(update.effective_message, reply_text, parse_mode=ParseMode.MARKDOWN)
+
 
 @run_async
 def log(update, context):
@@ -286,15 +154,11 @@ def deEmojify(inputString):
 
 __help__ = """
  - /stickerid: reply message sticker at PM to get ID sticker
- - /ping: check the speed of the bot
- - /fortune: give a fortune
- - /tr <from>-<to> <text>: translate text written or reply for any language to the intended language, or
- - /tr <to> <text>: translate text written or reply for any language to the intended language
  - /wiki <text>: search for text written from the wikipedia source
  - /ud <text>: search from urban dictionary
 """
 
-__mod_name__ = "💖 Exclusive Emilia 💖"
+__mod_name__ = "special"
 
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
 #GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker)
@@ -302,9 +166,8 @@ STIKER_HANDLER = CommandHandler("stiker", stiker, filters=Filters.user(OWNER_ID)
 FILE_HANDLER = CommandHandler("file", file, filters=Filters.user(OWNER_ID))
 GETLINK_HANDLER = CommandHandler("getlink", getlink, pass_args=True, filters=Filters.user(OWNER_ID))
 LEAVECHAT_HANDLER = CommandHandler(["leavechat", "leavegroup", "leave"], leavechat, pass_args=True, filters=Filters.user(OWNER_ID))
-TERJEMAH_HANDLER = DisableAbleCommandHandler(["tr", "tl"], trans_late)
-WIKIPEDIA_HANDLER = DisableAbleCommandHandler("wiki", wiki)
-UD_HANDLER = DisableAbleCommandHandler("ud", urbandictionary, pass_args=True)
+WIKIPEDIA_HANDLER = DisableAbleCommandHandler("wiki", wiki, pass_args=True)
+UD_HANDLER = DisableAbleCommandHandler("ud", urbandictionary)
 LOG_HANDLER = DisableAbleCommandHandler("log", log, filters=Filters.user(OWNER_ID))
 
 #dispatcher.add_handler(PING_HANDLER)
@@ -314,7 +177,6 @@ dispatcher.add_handler(STIKER_HANDLER)
 dispatcher.add_handler(FILE_HANDLER)
 dispatcher.add_handler(GETLINK_HANDLER)
 dispatcher.add_handler(LEAVECHAT_HANDLER)
-dispatcher.add_handler(TERJEMAH_HANDLER)
 dispatcher.add_handler(WIKIPEDIA_HANDLER)
 dispatcher.add_handler(UD_HANDLER)
 dispatcher.add_handler(LOG_HANDLER)
