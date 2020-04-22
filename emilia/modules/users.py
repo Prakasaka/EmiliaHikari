6be30b4d2c6f082f8639f774660f59e5ddc5c7cd
Prakasaka - the illuminator <heartbeatmwa@gmail.com>
2020-04-22 18:39:06 +0530

    f

--- emilia/modules/users.py
+++ emilia/modules/users.py
@@ -68,9 +68,8 @@ def broadcast(update, context):
 
 def slist(update, context):
     message = update.effective_message
-    if user_id == OWNER_ID:
+    for user_id in OWNER_ID:
         text = f"👑 Bot Owner:\n `{mention_html(user.id, user.first_name)}`"
-        return
     for user_id in SUDO_USERS:
         try:
             user = context.bot.get_chat(user_id)
