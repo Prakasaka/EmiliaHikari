5594080ef0703b9e5c5ac5877259a39d9419726f
Prakasaka - the illuminator <heartbeatmwa@gmail.com>
2020-04-27 00:43:05 +0530

    ok

--- emilia/modules/reporting.py
+++ emilia/modules/reporting.py
@@ -193,7 +193,7 @@ def button(bot, update):
                         InlineKeyboardButton("No", callback_data="ak_1+n|{}={}".format(report_chat, report_target))]
                 ]
                 reply_markup = InlineKeyboardMarkup(keyboard)
-                query.message.edit_text(text=msg + "\n\nAre you sure you want to kick {}?".format(userinfo.get('name')),
+                context.bot.editMessageText(text=msg + "\n\nAre you sure you want to kick {}?".format(userinfo.get('name')),
                                                   chat_id=query.message.chat_id,
                                                   message_id=query.message.message_id, parse_mode=ParseMode.HTML,
                                                   reply_markup=reply_markup)
@@ -203,7 +203,7 @@ def button(bot, update):
                         InlineKeyboardButton("No", callback_data="ak_2+n|{}={}".format(report_chat, report_target))]
                 ]
                 reply_markup = InlineKeyboardMarkup(keyboard)
-                query.message.edit_text(text=msg + "\n\nAre you sure you want to banned {}?".format(userinfo.get('name')),
+                context.bot.editMessageText(text=msg + "\n\nAre you sure you want to banned {}?".format(userinfo.get('name')),
                                                   chat_id=query.message.chat_id,
                                                   message_id=query.message.message_id, parse_mode=ParseMode.HTML,
                                                   reply_markup=reply_markup)
@@ -213,21 +213,21 @@ def button(bot, update):
                         InlineKeyboardButton("No", callback_data="ak_3+n|{}={}".format(report_chat, report_target))]
                 ]
                 reply_markup = InlineKeyboardMarkup(keyboard)
-                query.message.edit_text(text=msg + "\n\nDelete message?",
+                context.bot.editMessageText(text=msg + "\n\nDelete message?",
                                                   chat_id=query.message.chat_id,
                                                   message_id=query.message.message_id, parse_mode=ParseMode.HTML,
                                                   reply_markup=reply_markup)
         elif splitter[1] == "4":
                 try:
-                        query.message.edit_text(text=msg + "\n\nButton closed!",
+                        context.bot.editMessageText(text=msg + "\n\nButton closed!",
                                                   chat_id=query.message.chat_id,
                                                   message_id=query.message.message_id, parse_mode=ParseMode.HTML)
                 except Exception as err:
-                        query.message.edit_text(text=msg + "\n\nError: {}".format(err),
+                        context.bot.editMessageText(text=msg + "\n\nError: {}".format(err),
                                                   chat_id=query.message.chat_id,
                                                   message_id=query.message.message_id, parse_mode=ParseMode.HTML)
                 """
-                query.message.edit_text(text="Chat: {}\nAction: {}\nUser: {}".format(splitter[0], splitter[1], splitter[2]),
+                context.bot.editMessageText(text="Chat: {}\nAction: {}\nUser: {}".format(splitter[0], splitter[1], splitter[2]),
                                                   chat_id=query.message.chat_id,
                                                   message_id=query.message.message_id)
                 """
@@ -246,7 +246,7 @@ def buttonask(bot, update):
         if isyes == "y":
                 a, b = user_protection_checker(context.bot, report_target)
                 if not a:
-                        query.message.edit_text(text=msg + b,
+                        context.bot.editMessageText(text=msg + b,
                                                           chat_id=query.message.chat_id,
                                                           message_id=query.message.message_id, parse_mode=ParseMode.HTML)
                         return
@@ -256,11 +256,11 @@ def buttonask(bot, update):
                                 context.bot.sendMessage(report_chat, text="{} has been kicked!\nBy: {}".format(\
                                         mention_markdown(userinfo['id'], userinfo['name']), mention_markdown(chat.id, chat.first_name)), \
                                         parse_mode=ParseMode.MARKDOWN)
-                                query.message.edit_text(text=msg + "\n\n{} has been kicked!".format(mention_html(userinfo['id'], userinfo['name'])),
+                                context.bot.editMessageText(text=msg + "\n\n{} has been kicked!".format(mention_html(userinfo['id'], userinfo['name'])),
                                                           chat_id=query.message.chat_id,
                                                           message_id=query.message.message_id, parse_mode=ParseMode.HTML)
                         except Exception as err:
-                                query.message.edit_text(text=msg + "\n\nError: {}".format(err),
+                                context.bot.editMessageText(text=msg + "\n\nError: {}".format(err),
                                                           chat_id=query.message.chat_id,
                                                           message_id=query.message.message_id, parse_mode=ParseMode.HTML)
                 elif splitter[0] == "2":
@@ -269,25 +269,25 @@ def buttonask(bot, update):
                                 context.bot.sendMessage(report_chat, text="{} has been banned!\nBy: {}".format(\
                                         mention_markdown(userinfo['id'], userinfo['name']), mention_markdown(chat.id, chat.first_name)), \
                                         parse_mode=ParseMode.MARKDOWN)
-                                query.message.edit_text(text=msg + "\n\n{} has been banned!".format(mention_html(userinfo['id'], userinfo['name'])),
+                                context.bot.editMessageText(text=msg + "\n\n{} has been banned!".format(mention_html(userinfo['id'], userinfo['name'])),
                                                           chat_id=query.message.chat_id,
                                                           message_id=query.message.message_id, parse_mode=ParseMode.HTML)
                         except Exception as err:
-                                query.message.edit_text(text=msg + "\n\nError: {}".format(err),
+                                context.bot.editMessageText(text=msg + "\n\nError: {}".format(err),
                                                           chat_id=query.message.chat_id,
                                                           message_id=query.message.message_id, parse_mode=ParseMode.HTML)
                 elif splitter[0] == "3":
                         try:
                                 context.bot.deleteMessage(report_chat, report_target)
-                                query.message.edit_text(text=msg + "\n\nMessage was deleted!",
+                                context.bot.editMessageText(text=msg + "\n\nMessage was deleted!",
                                                           chat_id=query.message.chat_id,
                                                           message_id=query.message.message_id, parse_mode=ParseMode.HTML)
                         except Exception as err:
-                                query.message.edit_text(text=msg + "\n\nError: {}".format(err),
+                                context.bot.editMessageText(text=msg + "\n\nError: {}".format(err),
                                                           chat_id=query.message.chat_id,
                                                           message_id=query.message.message_id, parse_mode=ParseMode.HTML)
         elif isyes == "n":
-                query.message.edit_text(text=msg,
+                context.bot.editMessageText(text=msg,
                                                           chat_id=query.message.chat_id,
                                                           message_id=query.message.message_id, parse_mode=ParseMode.HTML,
                                                           reply_markup=key)
