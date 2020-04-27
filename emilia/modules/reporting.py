8cf07c5e87a6996f09e1cb6e008d53f9c9749c5a
Prakasaka - the illuminator <heartbeatmwa@gmail.com>
2020-04-27 22:50:42 +0530

    vc

--- emilia/modules/reporting.py
+++ emilia/modules/reporting.py
@@ -115,29 +115,32 @@ def report(update, context) -> str:
             if sql.user_should_report(admin.user.id):
                 all_admins.append("<a href='tg://user?id={}'>⁣</a>".format(admin.user.id))
                 try:
-                    if not chat.type == Chat.SUPERGROUP:
-                        context.bot.sendMessage(admin.user.id, msg, parse_mode=ParseMode.HTML)
+                    try:
+                        # context.bot.sendMessage(admin.user.id, msg, parse_mode=ParseMode.HTML)
                         if should_forward:
                             message.reply_to_message.forward(admin.user.id)
                             if len(message.text.split()) > 1:  # If user is giving a reason, send his message too
                                 message.forward(admin.user.id)
-                    if not chat.username:
-                        context.bot.sendMessage(admin.user.id, msg, parse_mode=ParseMode.HTML)
+                    except:
+                        pass
+                    context.bot.send_message(admin.user.id, msg, parse_mode=ParseMode.HTML, reply_markup=reply_markup)
+                    # if not chat.username:
+                    #     context.bot.sendMessage(admin.user.id, msg, parse_mode=ParseMode.HTML)
 
-                        if should_forward:
-                            message.reply_to_message.forward(admin.user.id)
+                    #     if should_forward:
+                    #         message.reply_to_message.forward(admin.user.id)
 
-                            if len(message.text.split()) > 1:  # If user is giving a reason, send his message too
-                                message.forward(admin.user.id)
+                    #         if len(message.text.split()) > 1:  # If user is giving a reason, send his message too
+                    #             message.forward(admin.user.id)
 
-                    if chat.username and chat.type == Chat.SUPERGROUP:
-                        context.bot.sendMessage(admin.user.id, msg, parse_mode=ParseMode.HTML, reply_markup=reply_markup)
+                    # if chat.username and chat.type == Chat.SUPERGROUP:
+                    #     context.bot.sendMessage(admin.user.id, msg, parse_mode=ParseMode.HTML, reply_markup=reply_markup)
 
-                        if should_forward:
-                            message.reply_to_message.forward(admin.user.id)
+                    #     if should_forward:
+                    #         message.reply_to_message.forward(admin.user.id)
 
-                            if len(message.text.split()) > 1:  # If user is giving a reason, send his message too
-                                message.forward(admin.user.id)
+                    #         if len(message.text.split()) > 1:  # If user is giving a reason, send his message too
+                    #             message.forward(admin.user.id)
 
                 except Unauthorized:
                     pass
