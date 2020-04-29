eb291a93f5a46cc08ac29eca5665a1273fa443aa
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2020-04-29 23:43:23 +0530

    latest code :p

--- emilia/modules/reporting.py
+++ emilia/modules/reporting.py
@@ -8,7 +8,7 @@ from telegram.ext import CommandHandler, MessageHandler, run_async, Filters, Cal
 from telegram.utils.helpers import mention_html, mention_markdown
 
 from emilia import dispatcher, LOGGER, OWNER_ID, SUDO_USERS, SUPPORT_USERS, STRICT_GBAN
-from emilia.modules.helper_funcs.chat_status import user_not_admin, user_admin, is_this_admin
+from emilia.modules.helper_funcs.chat_status import user_not_admin, user_admin, is_this_admin, is_user_ban_protected
 from emilia.modules.helper_funcs.extraction import extract_user
 from emilia.modules.log_channel import loggable
 from emilia.modules.sql import reporting_sql as sql
@@ -63,17 +63,21 @@ def report(update, context) -> str:
     args = context.args
     user_id = extract_user(message, args)
 
-    if user_id == OWNER_ID:
-        message.reply_text("are u reporting a bot owner? O bhai maaro mujhe maaro")
+    if is_user_ban_protected(chat, user_id):
+        message.reply_text("OOOH someone's trying to report a Admin! *grabs popcorn*")
         return
 
-    if int(user_id) in SUDO_USERS:
-        message.reply_text("OOOH someone's trying to report a sudo user! *grabs popcorn*")
-        return
+#     if user_id == OWNER_ID:
+#         message.reply_text("are u reporting a bot owner? O bhai maaro mujhe maaro")
+#         return
 
-    if user_id == context.bot.id:
-        message.reply_text("-_- So funny, lets report myself why don't I? Nice try.")
-        return
+#     if int(user_id) in SUDO_USERS:
+#         message.reply_text("OOOH someone's trying to report a sudo user! *grabs popcorn*")
+#         return
+
+#     if user_id == context.bot.id:
+#         message.reply_text("-_- So funny, lets report myself why don't I? Nice try.")
+#         return
 
     if is_this_admin(chat, user_id):
         message.reply_text("dude. it is admin of this group")
