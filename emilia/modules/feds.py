c651a3c9fb8ad6712ac3f1a1183a882939a434d6
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2020-05-11 22:41:16 +0530

    Update feds.py

--- emilia/modules/feds.py
+++ emilia/modules/feds.py
@@ -932,7 +932,6 @@ def fed_broadcast(update, context):
                 chat = update.effective_chat  # type: Optional[Chat]
                 fed_id = sql.get_fed_id(chat.id)
                 fedinfo = sql.get_fed_info(fed_id)
-                text = "*New broadcast from the Federation {}*\n".format(fedinfo['fname'])
                 # Parsing md
                 raw_text = msg.text
                 args = raw_text.split(None, 1)  # use python's maxsplit to separate cmd and args
@@ -941,7 +940,7 @@ def fed_broadcast(update, context):
                 text_parser = markdown_parser(txt,
                                       entities=msg.parse_entities(),
                                       offset=offset)
-                text += text_parser
+                text = text_parser
                 try:
                         broadcaster = user.first_name
                 except Exception:
@@ -950,12 +949,12 @@ def fed_broadcast(update, context):
                 chat_list = sql.all_fed_chats(fed_id)
                 failed = 0
                 for chat in chat_list:
+                    xt = "*New broadcast from the Federation {}*\n".format(fedinfo['fname'])
                     try:
-                        context.bot.sendMessage(chat, text, parse_mode="markdown")
+                        context.bot.sendMessage(chat, xt + text, parse_mode="markdown")
                     except TelegramError:
                         failed += 1
-                        LOGGER.warning("Couldn't send broadcast to %s, group name %s",
-                                           str(chat.chat_id), str(chat.chat_name))
+                        LOGGER.warning("Couldn't send broadcast to {}".format(str(chat)))
 
                 send_text = "The federation broadcast is complete"
                 if failed >= 1:
