35cee18f0991441f6f97ae876733f1977cbf03c4
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2020-05-22 16:26:57 +0530

    /help menu
    https://github.com/Nitin1818/Logan/blob/Nitin1818-update-1/haruka/modules/helper_funcs/misc.py

--- emilia/modules/helper_funcs/misc.py
+++ emilia/modules/helper_funcs/misc.py
@@ -38,7 +38,7 @@ def split_message(msg: str) -> List[str]:
 
         return result
 
-
+#https://github.com/Nitin1818/Logan/blob/Nitin1818-update-1/haruka/modules/helper_funcs/misc.py
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
         modules = sorted(
@@ -50,20 +50,30 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             [EqInlineKeyboardButton(x.__mod_name__,
                                     callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
              in module_dict.values()])
+    pairs = [
+        modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
+    ]
+    
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc == 1:
+        pairs.append((modules[-1], ))
+    elif calc == 2:
+        pairs.append((modules[-1], ))
 
-    pairs = list(zip(modules[::2], modules[1::2]))
+#     pairs = list(zip(modules[::2], modules[1::2]))
 
-    if len(modules) % 2 == 1:
-        pairs.append((modules[-1],))
+#     if len(modules) % 2 == 1:
+#         pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs))
-    modulo_page = page_n % max_num_pages
+#     max_num_pages = ceil(len(pairs))
+#     modulo_page = page_n % max_num_pages
 
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > 50:
-        pairs = pairs[modulo_page * 50:50 * (modulo_page + 1)]# + [
-            # (EqInlineKeyboardButton("<", callback_data="{}_prev({})".format(prefix, modulo_page)),
-            #  EqInlineKeyboardButton(">", callback_data="{}_next({})".format(prefix, modulo_page)))]
+#     # can only have a certain amount of buttons side by side
+#     if len(pairs) > 50:
+#         pairs = pairs[modulo_page * 50:50 * (modulo_page + 1)]# + [
+#             # (EqInlineKeyboardButton("<", callback_data="{}_prev({})".format(prefix, modulo_page)),
+#             #  EqInlineKeyboardButton(">", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     return pairs
 
